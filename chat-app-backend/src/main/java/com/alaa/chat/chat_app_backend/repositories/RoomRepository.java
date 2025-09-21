6599d34f9d38258266f358b7b829b155e45b4989
alaaSnayel <alaanayel111@gmail.com>
2025-09-21 10:52:45 +0300

    Backend: create initial packages and empty classes (controllers, repository)

---
+++ chat-app-backend/src/main/java/com/alaa/chat/chat_app_backend/repositories/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.alaa.chat.chat_app_backend.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.alaa.chat.chat_app_backend.entities.Room;
+
+public interface RoomRepository extends MongoRepository<Room, String>{
+
+  
+}
---
+++ chat-app-backend/src/main/java/com/alaa/chat/controller/ChatController.java
@@ -0,0 +1,5 @@
+package com.alaa.chat.controller;
+
+public class ChatController {
+
+}
