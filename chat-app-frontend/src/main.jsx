389705d1aa6e965f767a10504d1ca961c42f0815
alaaSnayel <alaanayel111@gmail.com>
2025-09-22 14:11:38 +0300

    Frontend: integrate React Router with app

--- chat-app-frontend/src/main.jsx
+++ chat-app-frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/chat" element={<h1>This is chat page</h1>} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
