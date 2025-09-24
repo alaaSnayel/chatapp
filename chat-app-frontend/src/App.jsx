e905daefe73e0d6c9bc2ac897e9eb6fda107390b
alaaSnayel <alaanayel111@gmail.com>
2025-09-24 12:39:21 +0300

    Frontend: install WebSocket libraries (STOMP & SockJS)

--- chat-app-frontend/src/App.jsx
+++ chat-app-frontend/src/App.jsx
@@ -1,7 +1,21 @@
 // import "./App.css";
 
+import toast from "react-hot-toast";
+
 function App() {
-  return <div className="">App page</div>;
+  return (
+    <div className="">
+      <h1>App page</h1>
+      <button
+        className=" bg-amber-400"
+        onClick={() => {
+          toast.success("dsfsfe");
+        }}
+      >
+        click me
+      </button>
+    </div>
+  );
 }
 
 export default App;
