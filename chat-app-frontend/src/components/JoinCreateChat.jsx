59c6bf9ee92ca9ceaf25a4146652ab2c811426d0
alaaSnayel <alaanayel111@gmail.com>
2025-09-25 11:23:59 +0300

    Frontend: Add form validation and error toast for empty fields in Join/Create Room

--- chat-app-frontend/src/components/JoinCreateChat.jsx
+++ chat-app-frontend/src/components/JoinCreateChat.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import chatIcon from "../assets/chat.png";
+import toast from "react-hot-toast";
 
 const JoinCreateChat = () => {
+  const [details, setDetails] = useState({
+    roomId: "",
+    userName: "",
+  });
+
+  function handleFormInputChange(event) {
+    setDetails({
+      ...details,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  function validateForm() {
+    if (details.userName === "" || details.roomId === "") {
+      toast.error("Please fill in all fields");
+      return false;
+    }
+    return true;
+  }
+
+  function joinChat() {
+    if (validateForm()) {
+      //Join Chat
+      console.log("wdad");
+    }
+  }
+
+  function createRoom() {
+    if (validateForm()) {
+      // Create Room
+      console.log("wdad");
+    }
+  }
+
   return (
     <div className="flex items-center justify-center h-screen bg-white text-gray-800 dark:bg-gray-950 dark:text-white">
       <div className="w-full max-w-md bg-gray-100 dark:bg-gray-900 p-8 rounded-2xl shadow-md">
@@ -19,7 +54,10 @@ const JoinCreateChat = () => {
           </label>
           <input
             id="name"
+            name="userName"
             type="text"
+            onChange={handleFormInputChange}
+            value={details.userName}
             placeholder="Enter your name"
             className="w-full px-4 py-2 rounded-lg border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
@@ -32,7 +70,10 @@ const JoinCreateChat = () => {
           </label>
           <input
             id="roomId"
+            name="roomId"
             type="text"
+            onChange={handleFormInputChange}
+            value={details.roomId}
             placeholder="Enter room ID"
             className="w-full px-4 py-2 rounded-lg border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
@@ -40,10 +81,16 @@ const JoinCreateChat = () => {
 
         {/* Buttons */}
         <div className="flex justify-center gap-4 mt-6">
-          <button className="w-1/2 cursor-pointer bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg transition">
+          <button
+            onClick={joinChat}
+            className="w-1/2 cursor-pointer bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg transition"
+          >
             Join Room
           </button>
-          <button className="w-1/2 cursor-pointer bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded-lg transition">
+          <button
+            onClick={createRoom}
+            className="w-1/2 cursor-pointer bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded-lg transition"
+          >
             Create Room
           </button>
         </div>
--- chat-app-frontend/src/main.jsx
+++ chat-app-frontend/src/main.jsx
@@ -8,7 +8,7 @@ import { Toaster } from "react-hot-toast";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Toaster position="top-right" />
+      <Toaster position="top-center" />
       <AppRoutes />
     </BrowserRouter>
   </StrictMode>
